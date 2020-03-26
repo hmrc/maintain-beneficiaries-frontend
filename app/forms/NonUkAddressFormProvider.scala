f803c34307cfc4954521205efe85f254966d8958
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-03-26 11:30:07 +0000

    TRUS-2321: Add Address pages and tests

---
+++ app/forms/NonUkAddressFormProvider.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.mappings.Mappings
+import javax.inject.Inject
+import models.NonUkAddress
+import play.api.data.{Form, Forms}
+import play.api.data.Forms._
+
+class NonUkAddressFormProvider @Inject() extends Mappings {
+
+  def apply(): Form[NonUkAddress] = Form(
+    mapping(
+      "line1" ->
+        text("nonUkAddress.error.line1.required")
+          .verifying(
+            firstError(
+              nonEmptyString("line1", "nonUkAddress.error.line1.required"),
+              maxLength(35, "nonUkAddress.error.line1.length"),
+              regexp(Validation.addressLineRegex, "nonUkAddress.error.line1.invalidCharacters")
+            )),
+      "line2" ->
+        text("nonUkAddress.error.line2.required")
+          .verifying(
+            firstError(
+              nonEmptyString("line2", "nonUkAddress.error.line2.required"),
+              maxLength(35, "nonUkAddress.error.line2.length"),
+              regexp(Validation.addressLineRegex, "nonUkAddress.error.line2.invalidCharacters")
+            )),
+      "line3" ->
+        optional(Forms.text
+          .verifying(
+            firstError(
+              maxLength(35, "nonUkAddress.error.line3.length"),
+              regexp(Validation.addressLineRegex, "nonUkAddress.error.line3.invalidCharacters")
+            ))),
+      "country" ->
+        text("nonUkAddress.error.country.required")
+          .verifying(
+            firstError(
+              maxLength(35, "nonUkAddress.error.country.length"),
+              nonEmptyString("country", "nonUkAddress.error.country.required")
+            ))
+    )(NonUkAddress.apply)(NonUkAddress.unapply)
+  )
+}
---
+++ test/forms/NonUkAddressFormProviderSpec.scala
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms
+
+import forms.behaviours.StringFieldBehaviours
+import play.api.data.FormError
+import wolfendale.scalacheck.regexp.RegexpGen
+
+class NonUkAddressFormProviderSpec extends StringFieldBehaviours {
+
+  val form = new NonUkAddressFormProvider()()
+
+  ".line1" must {
+
+    val fieldName = "line1"
+    val requiredKey = "nonUkAddress.error.line1.required"
+    val lengthKey = "nonUkAddress.error.line1.length"
+    val maxLength = 35
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      RegexpGen.from(Validation.addressLineRegex)
+    )
+
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+
+    behave like nonEmptyField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey, Seq(fieldName))
+    )
+  }
+
+  ".line2" must {
+
+    val fieldName = "line2"
+    val requiredKey = "nonUkAddress.error.line2.required"
+    val lengthKey = "nonUkAddress.error.line2.length"
+    val maxLength = 35
+
+    behave like fieldThatBindsValidData(
+      form,
+      fieldName,
+      RegexpGen.from(Validation.addressLineRegex)
+    )
+
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+
+    behave like nonEmptyField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey, Seq(fieldName))
+    )
+  }
+
+  ".line3" must {
+
+    val fieldName = "line3"
+    val lengthKey = "nonUkAddress.error.line3.length"
+    val maxLength = 35
+
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
+    )
+
+    behave like optionalField(
+      form,
+      fieldName,
+      validDataGenerator = RegexpGen.from(Validation.addressLineRegex)
+    )
+
+  }
+
+  ".country" must {
+
+    val fieldName = "country"
+    val requiredKey = "nonUkAddress.error.country.required"
+    val lengthKey = "nonUkAddress.error.country.length"
+    val maxLength = 35
+
+    behave like fieldWithMaxLength(
+      form,
+      fieldName,
+      maxLength = maxLength,
+      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+
+    behave like nonEmptyField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey, Seq(fieldName))
+    )
+  }
+}
