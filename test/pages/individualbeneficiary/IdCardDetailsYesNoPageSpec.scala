b597cee9b668c2c5791fc42f07b39224ee561982
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-03-26 09:14:32 +0000

    TRUS-2321: Add ID Card pages and tests

---
+++ test/pages/individualbeneficiary/IdCardDetailsYesNoPageSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pages.individualbeneficiary
+
+import java.time.LocalDate
+
+import models.Passport
+import pages.behaviours.PageBehaviours
+
+class IdCardDetailsYesNoPageSpec extends PageBehaviours {
+
+  val data: Passport = Passport("country", "number", LocalDate.of(2020, 1, 1))
+
+  "IdCardDetailsYesNo page" must {
+
+    beRetrievable[Boolean](IdCardDetailsYesNoPage)
+
+    beSettable[Boolean](IdCardDetailsYesNoPage)
+
+    beRemovable[Boolean](IdCardDetailsYesNoPage)
+
+    "implement cleanup logic when NO selected" in {
+      val userAnswers = emptyUserAnswers
+        .set(PassportDetailsPage, data)
+        .flatMap(_.set(IdCardDetailsYesNoPage, false))
+
+      userAnswers.get.get(IdCardDetailsPage) mustNot be(defined)
+    }
+  }
+}
