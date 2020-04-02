c6874303dd07959032e5e5cf1ab14e6e06305ad6
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-04-02 15:38:44 +0100

    TRUS-2360: Add RemoveCharityBeneficiary and WhenRemoved pages and tests

---
+++ app/controllers/charityortrust/remove/charity/RemoveCharityBeneficiaryController.scala
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.charityortrust.remove.charity
+
+import controllers.actions.StandardActionSets
+import forms.RemoveIndexFormProvider
+import javax.inject.Inject
+import models.{BeneficiaryType, RemoveBeneficiary}
+import pages.charityortrust.charity.RemoveYesNoPage
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.PlaybackRepository
+import services.TrustService
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import views.html.charityortrust.remove.charity.RemoveIndexView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RemoveCharityBeneficiaryController @Inject()(
+                                                       override val messagesApi: MessagesApi,
+                                                       repository: PlaybackRepository,
+                                                       standardActionSets: StandardActionSets,
+                                                       trustService: TrustService,
+                                                       formProvider: RemoveIndexFormProvider,
+                                                       val controllerComponents: MessagesControllerComponents,
+                                                       view: RemoveIndexView
+                                                  )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private val messagesPrefix: String = "removeCharityBeneficiary"
+
+  private val form = formProvider.apply(messagesPrefix)
+
+  def onPageLoad(index: Int): Action[AnyContent] = standardActionSets.identifiedUserWithData.async {
+    implicit request =>
+
+      val preparedForm = request.userAnswers.get(RemoveYesNoPage) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+
+      trustService.getCharityBeneficiary(request.userAnswers.utr, index).map {
+        beneficiary =>
+          Ok(view(preparedForm, index, beneficiary.name))
+      }
+
+  }
+
+  def onSubmit(index: Int): Action[AnyContent] = standardActionSets.identifiedUserWithData.async {
+    implicit request =>
+
+      import scala.concurrent.ExecutionContext.Implicits._
+
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) => {
+          trustService.getCharityBeneficiary(request.userAnswers.utr, index).map {
+            beneficiary =>
+              BadRequest(view(formWithErrors, index, beneficiary.name))
+          }
+        },
+        value => {
+
+          if (value) {
+
+            trustService.getCharityBeneficiary(request.userAnswers.utr, index).flatMap {
+              beneficiary =>
+                if (beneficiary.provisional) {
+                  trustService.removeBeneficiary(request.userAnswers.utr, RemoveBeneficiary(BeneficiaryType.CharityBeneficiary, index)).map(_ =>
+                    Redirect(controllers.routes.AddABeneficiaryController.onPageLoad())
+                  )
+                } else {
+                  for {
+                    updatedAnswers <- Future.fromTry(request.userAnswers.set(RemoveYesNoPage, value))
+                    _ <- repository.set(updatedAnswers)
+                  } yield {
+                    Redirect(controllers.charityortrust.remove.charity.routes.WhenRemovedController.onPageLoad(index))
+                  }
+                }
+            }
+          } else {
+            Future.successful(Redirect(controllers.routes.AddABeneficiaryController.onPageLoad()))
+          }
+        }
+      )
+  }
+}
