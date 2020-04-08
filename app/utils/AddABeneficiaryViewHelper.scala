f340f8ea78ccfce1b8e278dfb7766d90bfa6c78b
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-04-08 14:01:40 +0100

    Remove trust beneficiary.

--- app/utils/AddABeneficiaryViewHelper.scala
+++ app/utils/AddABeneficiaryViewHelper.scala
@@ -51,7 +51,7 @@ class AddABeneficiaryViewHelper(beneficiaries: Beneficiaries)(implicit messages:
       changeLabel = messages("site.change.details"),
       changeUrl = Some(controllers.routes.FeatureNotAvailableController.onPageLoad().url),
       removeLabel = messages("site.delete"),
-      removeUrl = Some(controllers.routes.FeatureNotAvailableController.onPageLoad().url)
+      removeUrl = Some(controllers.trust.remove.routes.RemoveTrustBeneficiaryController.onPageLoad(index).url)
     )
 
   private def renderCharityBeneficiary(beneficiary: CharityBeneficiary, index: Int) : AddRow =
