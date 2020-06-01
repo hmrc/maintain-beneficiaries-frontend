c3db19d544934b7a52d8bca5dfd30a4b911ff068
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2020-06-01 14:32:24 +0100

    TRUS-2630: Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,35 +1,42 @@
 import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
-  import play.core.PlayVersion
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"             %% "govuk-template"           % "5.52.0-play-26",
-    "uk.gov.hmrc"             %% "play-ui"                  % "8.8.0-play-26",
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.5.0",
+    "uk.gov.hmrc"             %% "govuk-template"           % "5.55.0-play-26",
+    "uk.gov.hmrc"             %% "play-ui"                  % "8.10.0-play-26",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.8.0",
     "com.typesafe.play"       %% "play-json-joda"           % "2.7.4",
-    "uk.gov.hmrc"             %% "domain"                   % "5.6.0-play-26",
-    "uk.gov.hmrc"             %% "play-whitelist-filter"    % "2.0.0",
+    "uk.gov.hmrc"             %% "domain"                   % "5.9.0-play-26",
+    "uk.gov.hmrc"             %% "play-whitelist-filter"    % "3.4.0-play-26",
     "org.reactivemongo"       %% "play2-reactivemongo"      % "0.18.8-play26"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.5.0" % Test classifier "tests",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.8.0" % Test classifier "tests",
     "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
-    "org.jsoup"               %  "jsoup"                    % "1.10.2"                % "test",
+    "org.jsoup"               %  "jsoup"                    % "1.12.1"                % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
     "org.mockito"             %  "mockito-all"              % "1.10.19"               % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test",
-    "org.scalacheck"          %% "scalacheck"               % "1.14.0"                % "test",
-    "wolfendale"              %% "scalacheck-gen-regexp"    % "0.1.1"                 % "test",
-    "com.github.tomakehurst"  % "wiremock-standalone"       % "2.17.0"                % "test"
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.3"                 % "test",
+    "org.scalacheck"          %% "scalacheck"               % "1.14.3"                % "test",
+    "wolfendale"              %% "scalacheck-gen-regexp"    % "0.1.2"                 % "test",
+    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.25.1"                % "test"
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
 
+  val akkaVersion = "2.5.23"
+  val akkaHttpVersion = "10.0.15"
+
+  val overrides = Seq(
+    "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+    "com.typesafe.akka" %% "akka-protobuf" % akkaVersion,
+    "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+    "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+    "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion
+  )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,7 +5,7 @@ resolvers += "HMRC Releases" at "https://dl.bintray.com/hmrc/releases"
 
 resolvers += Resolver.typesafeRepo("releases")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "2.6.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "2.7.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "2.1.0")
 
@@ -17,12 +17,12 @@ addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
 
 addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.4.13")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.20")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.25")
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.6.0")
 
-addSbtPlugin("net.ground5hark.sbt" % "sbt-concat" % "0.1.9")
+addSbtPlugin("net.ground5hark.sbt" % "sbt-concat" % "0.2.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-uglify" % "2.0.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-digest" % "1.1.2")
+addSbtPlugin("com.typesafe.sbt" % "sbt-digest" % "1.1.4")
