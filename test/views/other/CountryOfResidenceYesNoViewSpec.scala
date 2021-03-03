b1f26a54ed0ec061216d410ef045183070b592fa
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-03-03 12:17:40 +0000

    Added controllers, views and routes for other beneficiary country of residency questions.

---
+++ test/views/other/CountryOfResidenceYesNoViewSpec.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.other
+
+import controllers.other.routes
+import forms.YesNoFormProvider
+import models.NormalMode
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import views.behaviours.YesNoViewBehaviours
+import views.html.other.CountryOfResidenceYesNoView
+
+class CountryOfResidenceYesNoViewSpec extends YesNoViewBehaviours {
+
+  val messageKeyPrefix = "otherBeneficiary.countryOfResidenceYesNo"
+  val description: String = "Other"
+
+  val form: Form[Boolean] = new YesNoFormProvider().withPrefix(messageKeyPrefix)
+
+  "CountryOfResidenceYesNoView" must {
+
+    val view = viewFor[CountryOfResidenceYesNoView](Some(emptyUserAnswers))
+
+    def applyView(form: Form[_]): HtmlFormat.Appendable =
+      view.apply(form, NormalMode, description)(fakeRequest, messages)
+
+    behave like dynamicTitlePage(applyView(form), messageKeyPrefix, description)
+
+    behave like pageWithBackLink(applyView(form))
+
+    behave like yesNoPage(form, applyView, messageKeyPrefix, Some(description), routes.CountryOfResidenceYesNoController.onSubmit(NormalMode).url)
+
+    behave like pageWithASubmitButton(applyView(form))
+  }
+}
