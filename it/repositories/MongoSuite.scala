f52b41398d7e8b86b8b716b4dbba86480b8a08b7
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2021-04-13 09:47:06 +0100

    it-fix: Fix integration tests

--- it/repositories/MongoSuite.scala
+++ it/repositories/MongoSuite.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package repositories
 
 import org.scalatest.Assertion
@@ -8,7 +24,7 @@ import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.Helpers.running
 import play.modules.reactivemongo.ReactiveMongoApi
-import reactivemongo.api.MongoConnection
+import reactivemongo.api.{DefaultDB, MongoConnection}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -20,16 +36,16 @@ trait MongoSuite extends ScalaFutures {
   // Database boilerplate
   private val connectionString = "mongodb://localhost:27017/maintain-beneficiaries-frontend-it"
 
-  private def getDatabase(connection: MongoConnection) = {
+  private def getDatabase(connection: MongoConnection): Future[DefaultDB] = {
     connection.database("estates-store-integration")
   }
 
-  private def getConnection(application: Application) = {
+  private def getConnection(application: Application): Future[MongoConnection] = {
     val mongoDriver = application.injector.instanceOf[ReactiveMongoApi]
 
     for {
-      uri <- MongoConnection.parseURI(connectionString)
-      connection <- mongoDriver.driver.connection(uri, true)
+      uri <- MongoConnection.fromString(connectionString)
+      connection <- mongoDriver.asyncDriver.connect(uri)
     } yield connection
   }
 
@@ -49,11 +65,11 @@ trait MongoSuite extends ScalaFutures {
     ).build()
 
   def assertMongoTest(application: Application)(block: (Application, MongoConnection) => Assertion) : Future[Assertion] =
-    running(application) {
       for {
-        connection <- Future.fromTry(getConnection(application))
+        connection <- getConnection(application)
         _ <- dropTheDatabase(connection)
-      } yield block(application, connection)
+      } yield running(application) {
+        block(application, connection)
     }
 
 }
