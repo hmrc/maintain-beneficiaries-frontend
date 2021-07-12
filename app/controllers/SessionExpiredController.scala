bba84bfacd0a5f79f11f13da5f960ca510e458dc
“JoshuaH01” <34240122+JoshuaH01@users.noreply.github.com>
2021-07-12 09:55:08 +0100

    TRUS-4264| Fixed remaining views

--- app/controllers/SessionExpiredController.scala
+++ app/controllers/SessionExpiredController.scala
@@ -16,6 +16,7 @@
 
 package controllers
 
+import config.FrontendAppConfig
 import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
@@ -24,10 +25,15 @@ import views.html.SessionExpiredView
 
 class SessionExpiredController @Inject()(
                                           val controllerComponents: MessagesControllerComponents,
-                                          view: SessionExpiredView
+                                          view: SessionExpiredView,
+                                          appConfig: FrontendAppConfig
                                         ) extends FrontendBaseController with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = Action { implicit request =>
     Ok(view())
+  }
+
+  def onSubmit: Action[AnyContent] = Action { _ =>
+    Redirect(appConfig.loginUrl)
   }
 }
