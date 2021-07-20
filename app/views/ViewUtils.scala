9cbbae8e58977f320968dfb757748053ac56f27e
“JoshuaH01” <34240122+JoshuaH01@users.noreply.github.com>
2021-07-20 16:14:07 +0100

    TRUS-4264| Fixed focus issue on page when error message rendered

--- app/views/ViewUtils.scala
+++ app/views/ViewUtils.scala
@@ -40,8 +40,8 @@ object ViewUtils {
       case _ if radioOptions.size != 0 =>
         radioOptions.head.id
       case _ =>
-        val isSingleDateField = error.message.toLowerCase.contains("date") && !error.message.toLowerCase.contains("yesno")
-        if (error.key.toLowerCase.contains("date") || isSingleDateField) {
+        val isSingleDateField = isDateError(error.message) && !error.message.toLowerCase.contains("yesno")
+        if (isDateError(error.key) || isSingleDateField) {
           s"${error.key}.day"
         } else {
           s"${error.key}"
