38f5f11937ef3d51e94c2e1eca5ee1fc954b9f80
Ina Toni-Uebari <74660663+ina-tu@users.noreply.github.com>
2023-09-29 15:58:09 +0100

    DDCE-4614: updated character count component (#270)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.21.0"
+  val bootstrapVersion = "7.22.0"
   val mongoVersion = "1.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.20.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain"                         % "8.3.0-play-28",
@@ -16,13 +16,13 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.scalatest"               %% "scalatest"                % "3.2.16",
+    "org.scalatest"               %% "scalatest"                % "3.2.17",
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % mongoVersion,
-    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.17.0",
     "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "org.scalatestplus"           %% "mockito-4-11"             % "3.2.16.0",
-    "org.wiremock"                %  "wiremock-standalone"      % "3.0.0",
+    "org.scalatestplus"           %% "mockito-4-11"             % "3.2.17.0",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.2.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % "it, test")
 
