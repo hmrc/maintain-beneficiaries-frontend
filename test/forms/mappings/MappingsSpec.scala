b98c9c74c506a6aca7c0ec0eaee111cb9ab96465
Adam George <151747054+adageorg@users.noreply.github.com>
2024-01-22 11:27:27 +0000

    Adding filter to replace smart apostrophes with straight ones when binding string form controls.

--- test/forms/mappings/MappingsSpec.scala
+++ test/forms/mappings/MappingsSpec.scala
@@ -53,6 +53,11 @@ class MappingsSpec extends AnyWordSpec with Matchers with OptionValues with Mapp
       result.get mustEqual "foobar"
     }
 
+    "filter out smart apostrophes on binding" in {
+      val boundValue = testForm bind Map("value" -> "We’re ‘aving fish ‘n’ chips for tea")
+      boundValue.get mustEqual "We're 'aving fish 'n' chips for tea"
+    }
+
     "not bind an empty string" in {
       val result = testForm.bind(Map("value" -> ""))
       result.errors must contain(FormError("value", "error.required"))
