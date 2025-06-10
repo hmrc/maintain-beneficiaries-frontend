feac79ed9cb75b73b0f9bfbb8d7eb728e508dffa
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-06-10 15:50:22 +0100

    DDCE-3621 - reverting play frontend version and pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % mongoVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "9.11.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"       %% "domain-play-30"                         % "11.0.0"
   )
