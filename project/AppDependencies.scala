a0e7f6962539452b887e5d87784dfa322f4229ce
harish-korada1 <174133905+harish-korada1@users.noreply.github.com>
2026-03-13 15:58:37 +0000

    DDCE-8245:update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
   private val mongoVersion     = "2.12.0"
 
-  val compile: Seq[ModuleID] = Seq(
+  private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.27.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0"
   )
 
-  val test: Seq[ModuleID] = Seq(
+  private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoVersion,
     "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0",
